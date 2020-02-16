66b41320a74dd3c96de1c0a0a152e70bbe45f8d9
Slavik Urdzik <Urdziksv@gmail.com>
2020-02-16 07:24:51 +0200

    Added Book loading in RecyclerView

--- app/src/main/java/com/example/bookapp/view/HomeFragment.kt
+++ app/src/main/java/com/example/bookapp/view/HomeFragment.kt
@@ -2,22 +2,41 @@ package com.example.bookapp.view
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bookapp.BookContact
 import com.example.bookapp.R
+import com.example.bookapp.model.network.Book
+import com.example.bookapp.presentrer.BookPresenter
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), BookContact.View {
+    lateinit var adapter: MainAdapter
+    lateinit var presenter: BookPresenter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        presenter = BookPresenter(this)
+        adapter = MainAdapter(view.context)
+        view.rv.adapter = adapter
+
+        presenter.requestData()
+
         return view
+    }
+
+    override fun retrieveData(book: List<Book>) {
+        adapter.submitList(book)
+    }
+
+    override fun onError(t: Throwable) {
+        Log.d("one2", t.toString())
     }
 
 
