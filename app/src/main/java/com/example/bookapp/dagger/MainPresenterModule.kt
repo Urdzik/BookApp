f690517ea0ead48c72ee396d67de9281b800b513
Slavik Urdzik <urdziksv@gmail.com>
2020-03-06 09:15:47 +0200

    1

--- app/src/main/java/com/example/bookapp/dagger/MainPresenterModule.kt
+++ app/src/main/java/com/example/bookapp/dagger/MainPresenterModule.kt
@@ -3,13 +3,14 @@ package com.example.bookapp.dagger
 import com.example.bookapp.BookContact
 import com.example.bookapp.model.BookModel
 import dagger.Module
+
 import dagger.Provides
 
 @Module
 class MainPresenterModule(var view: BookContact.View) {
 
     @Provides
-    fun provideBookContact(): BookContact.View = view
+    fun provideBookContacts(): BookContact.View = view
 
     @Provides
     fun provideBookModule(): BookModel = BookModel()
